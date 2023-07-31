1539c0496ca39b4e6b5f9d3a67afddd653005a8c
dzwdz <kg67199@gmail.com>
2023-07-31 20:04:34 +0200

    add ./configure and a `make install` target

--- xdg.c
+++ xdg.c
@@ -1,24 +1,18 @@
 /** xdg.c
  * Figures out the potential locations of Lua files (inc. configs).
- * In order of preference:
- * 1. $XDG_CONFIG_HOME/hewwo/edge/
- * 2. /etc/hewwo/edge/
- * 3. the location of the executable
  */
 
+#include "config.h"
 #include "hewwo.h"
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define CFGDIR "/hewwo/edge/"
-
 static char *cpycat(char *dst, const char *src);
 static char *cpycatfree(char *dst, const char *src);
 static char *lpath_base(char *dst, const char *base);
 static char *config_path(void);
-static char *exedir(void);
 
 static char *
 cpycat(char *dst, const char *src)
@@ -56,11 +50,11 @@ config_path(void)
 	char *base;
 	base = getenv("XDG_CONFIG_HOME");
 	if (base) {
-		return cpycat(base, CFGDIR);
+		return cpycat(base, LUADIR_LOCAL);
 	}
 	base = getenv("HOME");
 	if (base) {
-		return cpycat(base, "/.config" CFGDIR);
+		return cpycat(base, "/.config" LUADIR_LOCAL);
 	}
 	return NULL;
 }
@@ -91,7 +85,7 @@ get_luapath(void)
 	char *cfg_path = config_path();
 
 	res = lpath_base(res, cfg_path);
-	res = lpath_base(res, "/etc" CFGDIR);
+	res = lpath_base(res, LUADIR_GLOBAL);
 	res = lpath_base(res, exedir());
 
 	free(cfg_path);
